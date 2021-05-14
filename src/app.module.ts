d86fc4aa1bb0b2043cc8b3267a826bad1e138618
kartheek.gade <gade.reddy@globallogic.com>
2021-05-14 10:34:57 +0530

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UserModule} from './user/user.module';
+
+
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot(
+      'mongodb://localhost:27017/mydb',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
